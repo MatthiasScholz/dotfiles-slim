d4e5654668d86288d303a413ba9445d6a3378c8b
Matthias Scholz <matthias.scholz@thoughtworks.com>
2026-02-03 12:50:38 +0100

    🚧 open CAD software

--- home/tinker.nix
+++ home/tinker.nix
@@ -11,6 +11,15 @@
     # freecad
     # NOTE not available for darwin systems -> brew
     # orca-slicer
+    # OpenSCAD
+    # . https://openscad.org
+    # UNUSED openscad
+    # UNUSED openscad-lsp
+    # PCB Design
+    # FIXME broken for darwin
+    # kicad
+    # Generate case out of pcb for openscad
+    # turbocase
   ];
 
 }
