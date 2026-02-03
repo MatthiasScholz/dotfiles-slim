2aa2258e8778b03eb4b591ac645a3404d492f976
Matthias Scholz <matthias.scholz@thoughtworks.com>
2026-02-03 12:53:46 +0100

    🚧 ffmpeg support

--- home/music.nix
+++ home/music.nix
@@ -1,8 +1,15 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
     spotify
+    # Audio converter
+    # FIXME 2025-11-18
+    # ffmpeg_8
   ];
-
 }
