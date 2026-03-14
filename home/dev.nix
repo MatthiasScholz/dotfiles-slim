3b0b19125d673a0be1b256edeac0f0b96795da3a
Matthias Scholz <matthias.scholz@thoughtworks.com>
2026-03-14 20:57:00 +0100

    :bug: wrong package hash and version reference

--- home/dev.nix
+++ home/dev.nix
@@ -8,13 +8,13 @@
 let
   # NOTE show how packages can be pinned to a specific nixpkgs version
   # unstable branch
-  pkgs_devenv_1_11_2 =
+  pkgs_devenv_2_0_0 =
     import
       (pkgs.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs";
-        rev = "f109adf791ee28d3bca68acbfaf0bdc9fe75dad8"; # 1.11.2
-        sha256 = "sha256-NWFyaZ4AWTVJzB+xYSkEgvtZ1YewsYe/2UtBTLi8PPg";
+        rev = "f109adf791ee28d3bca68acbfaf0bdc9fe75dad8"; # 2.0.0
+        sha256 = "sha256-s+qupP2eo4DK7jYtUdnPatbMjOYpUgdPvkiZWxw5kjk=";
       })
       {
         inherit (pkgs) system;
@@ -29,7 +29,7 @@ in
     # determinate-nixd upgrade --extra-conf " extra-substituters = https://devenv.cachix.org extra-trusted-public-keys =  devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     # FIXME latest devenv 2.0.3 broken
     # pkgks.devenv
-    pkgs_devenv_1_11_2.devenv
+    pkgs_devenv_2_0_0.devenv
     # NOTE not so nice to use because of JSON format and scripting in JSON.
     # alternative with hopefully better flake support
     # https://www.jetify.com/docs/devbox/guides/using_flakes/
