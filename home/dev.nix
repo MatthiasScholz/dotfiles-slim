856fe7360567a1e7f2ee2316ad8ff29cab580a0d
Matthias Scholz <matthias.scholz@thoughtworks.com>
2026-03-18 22:27:14 +0100

    :wastebasket: devenv got fixed with version 2.0.4.

--- home/dev.nix
+++ home/dev.nix
@@ -5,22 +5,6 @@
   ...
 }:
 
-let
-  # NOTE show how packages can be pinned to a specific nixpkgs version
-  # unstable branch
-  pkgs_devenv_2_0_0 =
-    import
-      (pkgs.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "f109adf791ee28d3bca68acbfaf0bdc9fe75dad8"; # 2.0.0
-        sha256 = "sha256-s+qupP2eo4DK7jYtUdnPatbMjOYpUgdPvkiZWxw5kjk=";
-      })
-      {
-        inherit (pkgs) system;
-        config.allowUnfree = true;
-      };
-in
 {
   home.packages = [
     # per project, vcs specific development environment setup
@@ -28,8 +12,8 @@ in
     # NOTE in order to use cachix binary caching the nix installations configuration has to be update:
     # determinate-nixd upgrade --extra-conf " extra-substituters = https://devenv.cachix.org extra-trusted-public-keys =  devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     # FIXME latest devenv 2.0.3 broken
-    # pkgks.devenv
-    pkgs_devenv_2_0_0.devenv
+    pkgs.devenv
+
     # NOTE not so nice to use because of JSON format and scripting in JSON.
     # alternative with hopefully better flake support
     # https://www.jetify.com/docs/devbox/guides/using_flakes/
